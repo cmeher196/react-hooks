d57706aa75144b4fa1732e74cac5ce7c9f0bc798
Chandra Meher <chamehe1@in.ibm.com>
2020-06-16 18:26:41 +0530

    useState with object

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import CountHook from './Components/CountHook';
 import HookCounterTwo from './Components/HookCounterTwo';
+import HookCounterThree from './Components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
       <h1>React Hooks!!</h1>
       {/* <CountHook /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree/>
     </div>
   );
 }
